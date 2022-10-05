64ce4a98d2b292ee4652dbcd1261d93615df1a7f
victor0812787 <victoragbaoso15@gmail.com>
2022-10-05 16:22:11 +0100

    charging the button

---
+++ js/app.js
@@ -0,0 +1,39 @@
+// STARTING first button
+let Hair = document.getElementById('button-one');
+let ears = document.getElementById('button-two');
+let eyes = document.getElementById('button-three');
+let mouth = document.getElementById('button-four');
+let neck = document.getElementById('button-five');
+let leg = document.getElementById('button-six');
+let accessories = document.getElementById('button-seven');
+let background = document.getElementById('button-eight');
+// ending first button
+
+// start second button
+let nine = document.getElementById('nine-button');
+let curls = document.getElementById('ten-button');
+let short = document.getElementById('eleven-button');
+let bang = document.getElementById('twelve-button');
+let elegant = document.getElementById('pick-button');
+let quiff = document.getElementById('fourteen-button');
+// end second button
+
+
+function h() {
+    nine.innerHTML = 'Curly'
+    curls.innerHTML = 'Thick'
+    short.innerHTML = 'Long'
+    bang.innerHTML = 'Short'
+    elegant.innerHTML = 'Matte'
+    quiff.innerHTML = 'straight'
+    elegant.add()
+
+}
+
+function ER() {
+    nine.innerHTML = 'Oblique'
+    curls.innerHTML = 'Rectangular'
+    short.innerHTML = 'Round'
+    bang.innerHTML = 'Triangular'
+
+}
